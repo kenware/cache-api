4a1b04ce1925328f396ab8a5d513fae48a34ad85
kenware <ejykken@gmail.com>
2021-04-10 22:40:03 +0100

    add readme.md

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # cache-api
+# Installation
+
+## Ensure you have node version 12.19.0
+## add .env and update it with variable on .env.sample
+## npm install
--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const TTL = process.env.TTL || 200;
 const mongoConnectionString = process.env.MONGO_URL;
-const maxCacheEntry = process.env.MAX_CACHE_ENTRY || 2;
+const maxCacheEntry = process.env.MAX_CACHE_ENTRY || 10;
 
 export default {
     TTL,
